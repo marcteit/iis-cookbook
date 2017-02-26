7926b41c61c8dd4b6eea27fceadb9f317e2628e2
Marc Teitelbaum <marcteit@gmail.com>
2017-02-25 23:31:22 -0300

    Importing WebAdministration module

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,9 +1,11 @@
 powershell_script 'Create Pool' do
+  code 'Import-Module \'WebAdministration\''
   code 'New-Item -Path IIS:\AppPools\[:AppPoolName]'
   not_if 'Test-Path IIS:\AppPools\[:AppPoolName]'
 end
 
 powershell_script 'Create App' do
+    code 'Import-Module \'WebAdministration\''
     code 'New-Item -Path IIS:\Sites\Default Web Site\[:AppName] -phyisicalPath c:\foo -Type Application'
     code 'Set-ItemProperty IIS:\Sites\Default Web Site\[:AppName] -name applicationPool -value [:AppPoolName]'
     not_if 'Test-Path IIS:\Sites\Default Web Site\[:AppName]'
