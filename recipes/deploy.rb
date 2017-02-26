de81ca347abd484cc85c41f7a17f64184420266a
Marc Teitelbaum <marcteit@gmail.com>
2017-02-26 00:47:46 -0300

    Environment variable read attempt

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,22 +1,22 @@
 powershell_script 'Create Pool' do
     code <<-EOH
         Import-Module WebAdministration
-        New-Item -Path IIS:\\AppPools\\#{AppPoolName}
+        New-Item -Path IIS:\\AppPools\\#{node[:deploy][0][:environment_variables][:AppPoolName]}
     EOH
     not_if <<-EOH
         Import-Module WebAdministration
-        Test-Path IIS:\\AppPools\\#{AppPoolName}
+        Test-Path IIS:\\AppPools\\#{node[:deploy][0][:environment_variables][:AppPoolName]}
     EOH
 end
 
 powershell_script 'Create App' do
     code <<-EOH
         Import-Module WebAdministration
-        New-Item -Path IIS:\\Sites\\Default Web Site\\#{AppName} -phyisicalPath c:\\foo -Type Application
-        Set-ItemProperty IIS:\\Sites\\Default Web Site\\#{AppName} -name applicationPool -value #{AppPoolName}
+        New-Item -Path IIS:\\Sites\\Default Web Site\\#{node[:deploy][0][:environment_variables][:AppName]} -phyisicalPath c:\\foo -Type Application
+        Set-ItemProperty IIS:\\Sites\\Default Web Site\\#{node[:deploy][0][:environment_variables][:AppName]} -name applicationPool -value #{node[:deploy][0][:environment_variables][:AppPoolName]}
     EOH
     not_if <<-EOH
         Import-Module WebAdministration
-        Test-Path IIS:\\Sites\Default Web Site\\#{AppName}
+        Test-Path IIS:\\Sites\Default Web Site\\#{node[:deploy][0][:environment_variables][:AppName]}
     EOH
 end
