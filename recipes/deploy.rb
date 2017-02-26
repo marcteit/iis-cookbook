cec8f9d6dada6f8692ca309554d7361230082a90
Marc Teitelbaum <marcteit@gmail.com>
2017-02-26 00:16:41 -0300

    Double back-slash

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,22 +1,22 @@
 powershell_script 'Create Pool' do
     code <<-EOH
         Import-Module WebAdministration
-        New-Item -Path IIS:\AppPools\[:AppPoolName]
+        New-Item -Path IIS:\\AppPools\\[:AppPoolName]
     EOH
     not_if <<-EOH
         Import-Module WebAdministration
-        Test-Path IIS:\AppPools\[:AppPoolName]
+        Test-Path IIS:\\AppPools\\[:AppPoolName]
     EOH
 end
 
 powershell_script 'Create App' do
     code <<-EOH
         Import-Module WebAdministration
-        New-Item -Path IIS:\Sites\Default Web Site\[:AppName] -phyisicalPath c:\foo -Type Application
-        Set-ItemProperty IIS:\Sites\Default Web Site\[:AppName] -name applicationPool -value [:AppPoolName]
+        New-Item -Path IIS:\\Sites\\Default Web Site\\[:AppName] -phyisicalPath c:\\foo -Type Application
+        Set-ItemProperty IIS:\\Sites\\Default Web Site\\[:AppName] -name applicationPool -value [:AppPoolName]
     EOH
     not_if <<-EOH
         Import-Module WebAdministration
-        Test-Path IIS:\Sites\Default Web Site\[:AppName]
+        Test-Path IIS:\\Sites\Default Web Site\\[:AppName]
     EOH
 end
