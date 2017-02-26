9f018dc4df585efcbba93481168eab50a1896982
Marc Teitelbaum <marcteit@gmail.com>
2017-02-25 23:42:49 -0300

    Code multiline fix

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,12 +1,22 @@
 powershell_script 'Create Pool' do
-  code 'Import-Module \'WebAdministration\''
-  code 'New-Item -Path IIS:\AppPools\[:AppPoolName]'
-  not_if 'Test-Path IIS:\AppPools\[:AppPoolName]'
+    code <<-EOH
+        Import-Module WebAdministration
+        New-Item -Path IIS:\AppPools\[:AppPoolName]
+    EOH
+    not_if <<-EOH
+        Import-Module WebAdministration
+        Test-Path IIS:\AppPools\[:AppPoolName]
+    EOH
 end
 
 powershell_script 'Create App' do
-    code 'Import-Module \'WebAdministration\''
-    code 'New-Item -Path IIS:\Sites\Default Web Site\[:AppName] -phyisicalPath c:\foo -Type Application'
-    code 'Set-ItemProperty IIS:\Sites\Default Web Site\[:AppName] -name applicationPool -value [:AppPoolName]'
-    not_if 'Test-Path IIS:\Sites\Default Web Site\[:AppName]'
+    code <<-EOH
+        Import-Module WebAdministration
+        New-Item -Path IIS:\Sites\Default Web Site\[:AppName] -phyisicalPath c:\foo -Type Application
+        Set-ItemProperty IIS:\Sites\Default Web Site\[:AppName] -name applicationPool -value [:AppPoolName]
+    EOH
+    not_if <<-EOH
+        Import-Module WebAdministration
+        Test-Path IIS:\Sites\Default Web Site\[:AppName]
+    EOH
 end
