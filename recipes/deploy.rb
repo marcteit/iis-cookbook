e19437c48b22f374b94540dee210cd80149fdde6
Marc Teitelbaum <marcteit@gmail.com>
2017-02-27 18:25:11 -0300

    No create app

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -9,14 +9,14 @@ powershell_script 'Create Pool' do
     EOH
 end
 
-powershell_script 'Create App' do
-    code <<-EOH
-        Import-Module WebAdministration
-        New-Item -Path IIS:\\Sites\\Default Web Site\\#{ENV['AppName']} -phyisicalPath c:\\foo -Type Application
-        Set-ItemProperty IIS:\\Sites\\Default Web Site\\#{ENV['AppName']} -name applicationPool -value #{ENV['AppPoolName']}
-    EOH
-    not_if <<-EOH
-        Import-Module WebAdministration
-        Test-Path IIS:\\Sites\Default Web Site\\#{ENV['AppName']}
-    EOH
-end
+#powershell_script 'Create App' do
+#    code <<-EOH
+#        Import-Module WebAdministration
+#        New-Item -Path IIS:\\Sites\\Default Web Site\\#{ENV['AppName']} -phyisicalPath c:\\foo -Type Application
+#        Set-ItemProperty IIS:\\Sites\\Default Web Site\\#{ENV['AppName']} -name applicationPool -value #{ENV['AppPoolName']}
+#    EOH
+#    not_if <<-EOH
+#        Import-Module WebAdministration
+#        Test-Path IIS:\\Sites\Default Web Site\\#{ENV['AppName']}
+#    EOH
+#end
