b6d9b86abf884d1b37ebf6c0f91f53ae7f8d2a87
Marc Teitelbaum <marcteit@gmail.com>
2017-03-02 17:59:11 -0300

    Fix

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,57 +1,52 @@
-log 'AppPoolName' do
-  message "#{node['AppPoolName']}"
-  level :info
-end
-
-log 'AppName' do
-  message "#{node['AppName']}"
-  level :info
-end
-
-log 'full_node' do
-    message "#{Chef::JSONCompat.to_json_pretty(node.to_hash)}"
-    level :info
-end
-
-#log 'zero' do
-#  message "#{node['deploy'][0]}"
+#log 'AppPoolName' do
+#  message "#{node['AppPoolName']}"
 #  level :info
 #end
 
-#powershell_script 'Create Pool' do
-#    code <<-EOH
-#        Import-Module WebAdministration
-#        New-Item -Path IIS:\\AppPools\\#{node['deploy'][0]['environment_variables']['AppPoolName']}
-#    EOH
-#    not_if <<-EOH
-#        Import-Module WebAdministration
-#        Test-Path IIS:\\AppPools\\#{node['deploy'][0]['environment_variables']['AppPoolName']}
-#    EOH
+#log 'AppName' do
+#  message "#{node['AppName']}"
+#  level :info
 #end
 
-#directory "c:\\#{node['deploy'][0]['environment_variables']['AppName']}" do
-#  action :create
-#  rights :read, 'Everyone'
-#  owner 'Administrator'
+#log 'full_node' do
+#    message "#{Chef::JSONCompat.to_json_pretty(node.to_hash)}"
+#    level :info
 #end
 
-#file "c:\\#{node['deploy'][0]['environment_variables']['AppName']}\\index.html" do
-#  rights :read, 'Everyone'
-#  content "<html>
-#  <body>
-#    <h1>Hello World: #{node['deploy'][0]['environment_variables']['AppName']}</h1>
-#  </body>
-#</html>"
-#end
+powershell_script 'Create Pool' do
+    code <<-EOH
+        Import-Module WebAdministration
+        New-Item -Path IIS:\\AppPools\\#{node['AppPoolName']}
+    EOH
+    not_if <<-EOH
+        Import-Module WebAdministration
+        Test-Path IIS:\\AppPools\\#{node['AppPoolName']}
+    EOH
+end
 
-#powershell_script 'Create App' do
-#    code <<-EOH
-#        Import-Module WebAdministration
-#        New-Item -Path 'IIS:\\Sites\\Default Web Site\\#{node['deploy'][0]['environment_variables']['AppName']}' -physicalPath c:\\#{node['deploy'][0]['environment_variables']['AppName']} -Type Application
-#        Set-ItemProperty 'IIS:\\Sites\\Default Web Site\\#{node['deploy'][0]['environment_variables']['AppName']}' -name applicationPool -value #{node['deploy'][0]['environment_variables']['AppPoolName']}
-#    EOH
-#    not_if <<-EOH
-#        Import-Module WebAdministration
-#        Test-Path 'IIS:\\Sites\Default Web Site\\#{node['deploy'][0]['environment_variables']['AppName']}'
-#    EOH
-#end
+directory "c:\\#{node['AppName']}" do
+  action :create
+  rights :read, 'Everyone'
+  owner 'Administrator'
+end
+
+file "c:\\#{node['AppName']}\\index.html" do
+  rights :read, 'Everyone'
+  content "<html>
+  <body>
+    <h1>Hello World: #{node['AppName']}</h1>
+  </body>
+</html>"
+end
+
+powershell_script 'Create App' do
+    code <<-EOH
+        Import-Module WebAdministration
+        New-Item -Path 'IIS:\\Sites\\Default Web Site\\#{node['AppName']}' -physicalPath c:\\#{node['AppName']} -Type Application
+        Set-ItemProperty 'IIS:\\Sites\\Default Web Site\\#{node['AppName']}' -name applicationPool -value #{node['AppPoolName']}
+    EOH
+    not_if <<-EOH
+        Import-Module WebAdministration
+        Test-Path 'IIS:\\Sites\Default Web Site\\#{node['AppName']}'
+    EOH
+end
